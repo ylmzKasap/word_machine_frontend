fb764b26aadf3c3a09cd4debc68c4592849a52e7
Yılmaz Kasap <ylmzkasap@outlook.com>
2023-01-04 19:25:17 +0300

    Tweak image upload regexes

--- src/profile_components/common/regex.ts
+++ src/profile_components/common/regex.ts
@@ -1,6 +1,6 @@
 export const specialCharacterRegex = /[~#@\\<>|"().,;=]/;
-export const artistNameRegex = /[~#\\<>|"().,;]/;
+export const artistNameRegex = /[~#\\<>|"(),;]/;
 export const itemNameFilter = /[.,\\<>"]/;
-export const urlRegex = /[^A-Za-z0-9\\\-._~:/?#[\]@!$&'()*+,;=]/;
+export const urlRegex = /[^A-Za-z0-9\\\-._~:/?#[\]@!%$&'()*+,;=]/;
 export const imageNameRegex = /\.(jpg|jpeg|jfif|png|webp)$/i;
 export const soundNameRegex = /\.mp3$/i;
